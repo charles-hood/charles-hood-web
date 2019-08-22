4f4ab4b23f3b73b2467b48f4ea1636f05a477a69
Charles Hood <charles.hood@gmail.com>
2019-08-21 20:50:31 -0400

    Updated copyright

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -5,7 +5,7 @@ export default function PageFooter() {
   return (
     <footer id="footer">
       <ul className="copyright">
-        <li>&copy; {config.authorName}</li>
+        <li>&copy; 2019 {config.authorName}</li>
         <li>
           Design: <a href="https://unsplash.com/">Unsplash</a> +{' '}
           <a href="http://html5up.net">HTML5 UP</a>
